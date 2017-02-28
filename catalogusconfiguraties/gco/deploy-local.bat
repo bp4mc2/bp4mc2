3600c56f0b3529e5c1878cbd321303981fcd04e4
architolk <msj@xs4all.nl>
2017-02-28 23:29:58 +0100

    Fixed for local deployment without docroot

--- catalogusconfiguraties/gco/deploy-local.bat
+++ catalogusconfiguraties/gco/deploy-local.bat
@@ -1,11 +1,13 @@
 @echo off
 echo Clearing graph...
 curl.exe -X PUT -T empty.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/gco/stage
+curl.exe -X PUT -T empty.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/catalogus/stage
 type NUL > "Stage GCO local.ttl"
 cd onderdelen
 for %%y in (*.ttl) do (
 	echo Processing %%y...
 	curl.exe -X POST -T %%y http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/gco/stage
+	curl.exe -X POST -T %%y http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/catalogus/stage
 	type %%y >> "../Stage GCO local.ttl"
 	echo. >> "../Stage GCO local.ttl"
 )
