0b6179e38040a515a35215232bd6783aac96ca09
architolk <msj@xs4all.nl>
2017-06-21 08:16:05 +0200

    KKG mapping overzicht toegevoegd

--- model/controle-local-kkg.bat
+++ model/controle-local-kkg.bat
@@ -1,5 +1,7 @@
 @echo off
 echo KKG model
-curl.exe -X PUT -T kkg/kkg.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://bp4mc2.org/id/dataset/kkg
+curl.exe -X PUT -T kkg/KKG.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://bp4mc2.org/id/dataset/kkg
+echo KKG links
+curl.exe -X POST -T kkg/KKGpathnames.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://bp4mc2.org/id/dataset/kkg
 pause
 
