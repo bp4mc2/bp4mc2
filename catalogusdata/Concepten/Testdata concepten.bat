0f85098833ea109a94b89c95cd315084cb4bfed8
Wortel <lars.wortel@capgemini.com>
2017-06-28 17:35:59 +0200

    Oplevering GCO-499 en GCO-473

--- catalogusdata/Concepten/Testdata concepten.bat
+++ catalogusdata/Concepten/Testdata concepten.bat
@@ -2,8 +2,8 @@
 echo Creating concatenated file O...
 powershell -Command "$myFile = (gc 'Testdata concepten local.ttl') -replace 'http://localhost:8080', 'http://data.ontwikkeling.pdok.nl'; [System.IO.File]::WriteAllLines('Testdata concepten O.ttl', $myFile)"
 echo Creating concatenated file T...
-powershell -Command "$myFile = (gc 'Testdata concepten local.ttl') -replace 'http://localhost:8080', 'http://data.test.pdok.nl'; [System.IO.File]::WriteAllLines('Testdata concepten O.ttl', $myFile)"
+powershell -Command "$myFile = (gc 'Testdata concepten local.ttl') -replace 'http://localhost:8080', 'http://data.test.pdok.nl'; [System.IO.File]::WriteAllLines('Testdata concepten T.ttl', $myFile)"
 echo Creating concatenated file A...
-powershell -Command "$myFile = (gc 'Testdata concepten local.ttl') -replace 'http://localhost:8080', 'http://data.acceptatie.pdok.nl'; [System.IO.File]::WriteAllLines('Testdata concepten O.ttl', $myFile)"
+powershell -Command "$myFile = (gc 'Testdata concepten local.ttl') -replace 'http://localhost:8080', 'http://data.acceptatie.pdok.nl'; [System.IO.File]::WriteAllLines('Testdata concepten A.ttl', $myFile)"
 echo Whoohoo! All done!
 pause
