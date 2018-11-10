7cef1532291e43b9b414ec80ef5a0718e70c45f7
architolk <msj@xs4all.nl>
2018-11-10 16:29:01 +0100

    Fixed publish model for new location

--- publish-model.sh
+++ publish-model.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo Publish markdown documents
 cd publication
-for f in *.md ; do echo ${f%.md}; cp $f ../../bp4mc2.github.io/model ; done
+for f in *.md ; do echo ${f%.md}; cp $f ../../bp4mc2.github.io/profiles ; done
 echo Publish english markdown documents
 cd en
-for f in *.md ; do echo ${f%.md}; cp $f ../../../bp4mc2.github.io/model/en ; done
+for f in *.md ; do echo ${f%.md}; cp $f ../../../bp4mc2.github.io/profiles/en ; done
 cd ../..
 echo Publish diagrams
 cd diagrams
-for f in *.png ; do echo ${f%.md}; cp $f ../../bp4mc2.github.io/model ; done
+for f in *.png ; do echo ${f%.md}; cp $f ../../bp4mc2.github.io/profiles ; done
 echo Publish diagrams to english site
-for f in *.png ; do echo ${f%.md}; cp $f ../../bp4mc2.github.io/model/en ; done
+for f in *.png ; do echo ${f%.md}; cp $f ../../bp4mc2.github.io/profiles/en ; done
 cd ..
