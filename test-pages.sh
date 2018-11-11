a8f93825a88a5635d65146ee7f242e9ca6c57b8a
architolk <msj@xs4all.nl>
2018-11-11 10:40:48 +0100

    Fixed frbr references

--- test-pages.sh
+++ test-pages.sh
@@ -2,3 +2,7 @@
 echo Copy markdown documents
 cd publication
 for f in *.md ; do echo ${f%.md}; cp $f /opt/tomcat7/webapps/ROOT/WEB-INF/resources/apps/profiles ; done
+cd ..
+echo Publish diagrams
+cd diagrams
+for f in *.png ; do echo ${f%.md}; cp $f /opt/tomcat7/webapps/ROOT/WEB-INF/resources/apps/profiles ; done
